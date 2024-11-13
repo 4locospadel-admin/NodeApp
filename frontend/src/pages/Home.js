b246decc0a46d1d38cd2bc02c5de6be675e8e69a
Michal Floch <4locospadel@gmail.com>
2024-11-13 16:10:14 +0100

    Final Cleanup and Deployment Pipeline Test

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -5,12 +5,11 @@ import './Home.css';
 function Home() {
     return (
         <div className="home">
-            <div className="button-container">
-                <div>AAAAAAAAAAAAAAA</div>
+            <div className="home-button-container">
                 <img src="/logo.png" alt="Logo" className="logo" />
-                <Link to="/database-test"><button className="button">DB Test</button></Link>
-                <Link to="/contact"><button className="button">Contact Form</button></Link>
-                <Link to="/about"><button className="button">About</button></Link>
+                <Link to="/database-test"><button className="home-button">DB Test</button></Link>
+                <Link to="/contact"><button className="home-button">Contact Form</button></Link>
+                <Link to="/about"><button className="home-button">About</button></Link>
             </div>
         </div>
     );
