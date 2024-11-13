eef9d124086590f39e3adf01abf0452b70f1e773
Michal Floch <4locospadel@gmail.com>
2024-11-13 11:32:53 +0100

    YAML Fix

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,14 @@
 const express = require('express');
 const path = require('path');
-
 const app = express();
-const PORT = process.env.PORT || 5001;
+const PORT = process.env.PORT || 8080;
 
 // Serve the static files from the React app
-app.use(express.static(path.join(__dirname, '../frontend/build')));
+app.use(express.static(path.join(__dirname, 'build')));
 
-// Handle any routes that don't match the API routes by serving `index.html`
+// Handle any requests that don’t match the API routes by serving index.html
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
 app.listen(PORT, () => {
