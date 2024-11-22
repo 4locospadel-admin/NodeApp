8e11e58dcc34222fa6ce95b53616ea4677071249
Michal Floch <4locospadel@gmail.com>
2024-11-22 13:26:18 +0100

    Profile page update

---
+++ frontend/src/pages/Profile.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div className="page">
+      <h2>Profile page</h2>
+      <p>
+        Here the user should be able to log in/sign up and then update his info.
+      </p>
+    </div>
+  );
+}
+
+export default Profile;
